069a21f98a0b9acd1e57e90f670143930dde0255
Ansul <ansulsingh171@gmail.com>
2025-04-07 08:33:40 +0530

    done

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import messageRoutes from './routes/message.route.js';
+import cors from 'cors';
+import {app,server} from './lib/socket.js'
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages',messageRoutes);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
